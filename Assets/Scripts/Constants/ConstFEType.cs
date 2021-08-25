c2d10ff971b148a2ca727d37df3631145512b867
Harry Sadler <harrysadlermusic@gmail.com>
2021-08-25 22:03:23 +0200

    add factory-entity constants

--- Assets/Scripts/Constants/ConstFEType.cs
+++ Assets/Scripts/Constants/ConstFEType.cs
@@ -16,14 +16,36 @@ public class ConstFEType
     public const int STONE = 103;
     public const int METAL = 104;
     public const int ORGANICS = 105;
+    public const int NITROGEN = 106;
+    public const int HELIUM = 107;
+    public const int CO2 = 108;
+    public const int HYDROGEN = 109;
+    public const int OXYGEN = 110;
+    public const int SILICATES = 111;
+    public const int QUARTZ = 112;
+    public const int IRON = 113;
+    public const int COPPER = 114;
+    public const int ALUMINUM = 115;
+    public const int LEAD = 116;
+    public const int RARE_METALS = 117;
+    public const int BIOMASS = 118;
+    public const int CELL_CULTURE = 119;
+
 
     // factory structures types
     public const int HARVESTER = 201;
     public const int DISTRIBUTOR = 202;
     public const int STORAGE = 203;
-    public const int MIRROR = 205;
-    public const int PHOTOVOLTAIC = 206;
-    public const int ACCUMULATOR = 207;
+    public const int RAW_RESOURCE_PROCESSOR = 204;
+    public const int FURNACE = 205;
+    public const int REFINERY = 206;
+    public const int ASSEMBLER = 207;
+    public const int BIOLAB = 208;
+    public const int BIOMASS_BOILER = 209;
+    public const int MIRROR = 210;
+    public const int PHOTOVOLTAIC = 211;
+    public const int ACCUMULATOR = 212;
+
 
     // factory unit types
     public const int WORKER = 301;
